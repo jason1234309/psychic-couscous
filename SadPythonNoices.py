e438551b97e8c28ef80d8932cc04819df2cde64b
Jason Prescott <64401388+jason1234309@users.noreply.github.com>
2021-06-11 17:38:43 -0600

    Delete SadPythonNoices.py

--- SadPythonNoices.py
+++
@@ -1,9 +0,0 @@
-print("What sounds does a sad python make?")
-
-# importing Image class from PIL package 
-from PIL import Image 
-  
-# creating a object 
-im = Image.open(https://github.com/jason1234309/psychic-couscous/blob/main/python_joke.html) 
-  
-im.show()
