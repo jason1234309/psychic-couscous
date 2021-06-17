1221825a8f5677eadc085404828d10235a86ca55
Jason <jason1234309@gmail.com>
2021-06-16 19:45:26 -0600

    here is the results of my script expirementing, also cleaning up unnecisary files

--- part_hardware/get_part_info.tcl
+++ part_hardware/get_part_info.tcl
@@ -1,86 +1,9 @@
-proc get_cell_information {cell} {
-    set cell_information_string $cell
-    set cell_type [get_property REF_NAME $cell]
+set part_name [lindex $argv 0]
 
-    set sites_used ""
-    set alternate_sites ""
-    set foo ""
+link_design -part $part_name
 
-    set loc [get_property "LOC" $cell]
-    if { $loc != ""} {
-        set tile_name [get_tiles -of $loc]
-        if {[get_property TILE_TYPE $tile_name] != ""} {
-            set tile_type [get_property TILE_TYPE $tile_name]
-        } else {
-            set tile_type "Tile does not have a type"
-        }
-    } else {
-        set tile_name "Cell does not have a Tile"
-        set tile_type ""
-    }
-    
-    if {[get_sites -of_object $cell] != ""} {
-        set site_name [get_sites -of_object $cell]
-        if {[get_property SITE_TYPE [get_sites -of_objects $cell]] != ""} {
-            set site_type [get_property SITE_TYPE [get_sites -of_objects $cell]]
-        } else {
-            set site_type "Site does not have a type"
-        }
-    } else {
-        set site_name "Cell does not have a Site"
-        set site_type ""
-    }
+set f [open part_data.lst w]
 
-    if { [get_sites -of_objects $cell] != "" } {
-        set potential_types [get_property SITE_TYPE [get_sites -of_objects $cell]]
-        if {[get_property  ALTERNATE_SITE_TYPES [get_sites -of_objects $cell]] != ""} {
-            set alternate_sites [get_property  ALTERNATE_SITE_TYPES [get_sites -of_objects $cell]]
-        } else {
-           set alternate_sites "" 
-        }
-        foreach S $alternate_sites {
-            lappend potential_types $S
-        }
-        foreach T $potential_types {
-            if {[catch {set_property MANUAL_ROUTING $T [get_sites -of_objects $cell]} foo] == 0 && $T != ""} {
-                lappend sites_used $T
-            }
-            reset_property MANUAL_ROUTING [get_sites -of_objects $cell]
-        }
-    }
-    if {[get_bels -of_objects $cell] != ""} {
-        set bel [get_bels -of_objects $cell]
-    } else {
-        set bel "Cell does not have a BEL"
-    }
+puts $f "$part_name, stuff, morestuff, some more stuff"
 
-    append cell_information_string ", $cell_type, $tile_name, $tile_type, $site_name, $site_type, $bel"
-    append cell_information_string ", "
-    append cell_information_string [join $sites_used "/"]
-    append cell_information_string ", "
-    append cell_information_string [join $alternate_sites "/"]
-
-    return $cell_information_string
-}
-
-
-proc get_part_information {part} {
-    link_design -part $part
-
-    set f [open cell_information.csv w]
-
-    set header "Cell Name, Cell Type, Tile Name, Tile Type, Site Name, Site Type, BEL, Site Used, Alt Site\n"
-    puts $f $header
-
-    foreach C [get_cells -hierarchical] {
-        set current_cell [get_cell_information $C]
-        if {[get_property LOC $C] != ""} {
-            puts $f $current_cell
-        }
-    }
-
-    close $f
-
-    puts "Done"
-}
-
+close $f
--- part_hardware/part_info_gen.py
+++ part_hardware/part_info_gen.py
@@ -10,14 +10,24 @@ current_directory = os.getcwd()
 
 data_rows = ""
 
-header = ["Part", "I/O Pin Count", "Available IOBs", "LUT Elements", "FlipFlops", "Block RAMs", "DSPs"]
-
-pd.read_csv('Table.xlsx')
-
-
-
+header = "Part, I/O Pin Count, Available IOBs, LUT Elements, FlipFlops, Block RAMs, DSPs"
 
 sys.stdout = open('part_hardware_list.csv', 'w')               
 print(header)
-print(data_rows)
+
+current_table = pd.read_csv('parts_csv.csv')
+partNames = current_table['Part']
+
+for part in partNames:
+    current_part = part
+    os.system('vivado -mode batch -nolog -nojournal -source get_part_info.tcl -tclargs ' + current_part)
+    with open('part_data.lst') as part_data:
+        lines = part_data.read()
+        print(lines)
+
+
+
+
+
+
 sys.stdout.close()
